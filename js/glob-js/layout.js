ca385f1c98028387f6248a6efaf0643adf3ff643
Vasprod <svasilceva@gmail.com>
2025-06-07 14:34:47 +0300

    fix page paths

--- js/glob-js/layout.js
+++ js/glob-js/layout.js
@@ -1,10 +1,10 @@
 export const episodeBackgrounds = {
-  1: '/img/episode1.jpg',
-  2: '/img/episode2.jpg',
-  3: '/img/episode3.jpg',
-  4: '/img/episode4.jpg',
-  5: '/img/episode5.jpg',
-  6: '/img/episode6.jpg'
+  1: '../../img/episode1.jpg',
+  2: '../../img/episode2.jpg',
+  3: '../../img/episode3.jpg',
+  4: '../../img/episode4.jpg',
+  5: '../../img/episode5.jpg',
+  6: '../../img/episode6.jpg'
 }
 
 export function createLayout(episodeId, title, episodeNumber, blockCard) {
